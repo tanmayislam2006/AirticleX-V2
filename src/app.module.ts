84680fb1a0a0b23c113a0c0bf4a2b2cec319adf9
tanmay2006 <tanmayislam101@gmail.com>
2026-01-15 01:14:45 +0600

    added auth and complete the auth route

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,11 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PostModule } from './post/post.module';
 import { CommentModule } from './comment/comment.module';
+import { AuthModule } from '@thallesp/nestjs-better-auth';
+import { auth } from './libs/auth';
 
 @Module({
-  imports: [PostModule, CommentModule],
+  imports: [PostModule, CommentModule, AuthModule.forRoot(auth)],
   controllers: [AppController],
   providers: [AppService],
 })
