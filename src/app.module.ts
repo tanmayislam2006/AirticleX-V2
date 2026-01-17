25ff7a320883bf485263d126e9ee72dfd7b8c142
tanmay2006 <tanmayislam101@gmail.com>
2026-01-17 23:41:19 +0600

    refactor: remove Comment module and related files; add Comments module with controller and service

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,12 +2,12 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PostModule } from './post/post.module';
-import { CommentModule } from './comment/comment.module';
 import { AuthModule } from '@thallesp/nestjs-better-auth';
 import { auth } from './libs/auth';
+import { CommentsModule } from './comments/comments.module';
 
 @Module({
-  imports: [PostModule, CommentModule, AuthModule.forRoot(auth)],
+  imports: [PostModule, AuthModule.forRoot(auth), CommentsModule],
   controllers: [AppController],
   providers: [AppService],
 })
