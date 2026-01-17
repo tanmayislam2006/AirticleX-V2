46eceedf4f7836e4d0746198631f271424b1e426
tanmay2006 <tanmayislam101@gmail.com>
2026-01-17 22:32:35 +0600

    added create  post route

--- src/post/post.controller.ts
+++ src/post/post.controller.ts
@@ -1,4 +1,16 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { PostService } from './post.service';
+import { Post as PostModel } from 'src/generated/prisma/client';
 
-@Controller('post')
-export class PostController {}
+@Controller('api/posts')
+export class PostController {
+  constructor(private readonly postService: PostService) {}
+  @Post()
+  createPost(
+    @Body()
+    postData: Omit<PostModel, 'id' | 'createdAt' | 'updatedAt' | 'authorID'>,
+  ) {
+    const userID = 'JuY5S9WEPWr19EHXTennC7LvbBt9ORN8';
+    return this.postService.createPost(postData, userID);
+  }
+}
--- src/post/post.service.ts
+++ src/post/post.service.ts
@@ -1,4 +1,19 @@
 import { Injectable } from '@nestjs/common';
+import { Post } from 'src/generated/prisma/client';
+import { prisma } from 'src/libs/prisma';
 
 @Injectable()
-export class PostService {}
+export class PostService {
+  createPost = async (
+    data: Omit<Post, 'id' | 'createdAt' | 'updatedAt' | 'authorID'>,
+    userID: string,
+  ) => {
+    const result = await prisma.post.create({
+      data: {
+        ...data,
+        authorID: userID,
+      },
+    });
+    return result;
+  };
+}
