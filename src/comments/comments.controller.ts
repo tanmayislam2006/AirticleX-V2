7ca80e15d998d2ba112f34c42ab2e7e9d6bd31c3
tanmay2006 <tanmayislam101@gmail.com>
2026-01-19 14:27:43 +0600

    feat: add updateComment method in CommentsService and corresponding route in CommentsController

--- src/comments/comments.controller.ts
+++ src/comments/comments.controller.ts
@@ -1,5 +1,6 @@
-import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import { Body, Controller, Get, Param, Patch, Post } from '@nestjs/common';
 import { CommentsService } from './comments.service';
+import { CommentStatus } from 'src/generated/prisma/enums';
 
 @Controller('comments')
 export class CommentsController {
@@ -23,5 +24,17 @@ export class CommentsController {
   @Get('/author/:authorID')
   getCommentsByAuthor(@Param('authorID') authorID: string) {
     return this.commentService.getCommentsByAuthor(authorID);
+  }
+  @Patch('/:commentID')
+  updateComment(
+    @Param() commentID: string,
+    @Body()
+    data: {
+      content?: string;
+      status?: CommentStatus;
+    },
+  ) {
+    const userID: string = 'JuY5S9WEPWr19EHXTennC7LvbBt9ORN8';
+    return this.commentService.updateComment(userID, commentID, data);
   }
 }
--- src/comments/comments.service.ts
+++ src/comments/comments.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@nestjs/common';
+import { CommentStatus } from 'src/generated/prisma/enums';
 import { prisma } from 'src/libs/prisma';
 
 @Injectable()
@@ -39,6 +40,35 @@ export class CommentsService {
       where: {
         authorID,
       },
+    });
+  };
+  updateComment = async (
+    authorID: string,
+    commentID: string,
+    data: { content?: string; status?: CommentStatus },
+  ) => {
+    const isOwner = await prisma.comment.findFirst({
+      where: {
+        id: commentID,
+        authorID,
+      },
+      select: {
+        id: true,
+        status: true,
+      },
+    });
+    if (!isOwner) {
+      throw new Error('Your provided input is invalid!');
+    }
+    if (isOwner.status === data.status) {
+      throw new Error(`Your provided status ${data.status} is up to date`);
+    }
+    return await prisma.comment.update({
+      where: {
+        id: commentID,
+        authorID,
+      },
+      data,
     });
   };
 }
